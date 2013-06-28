f7bafd15e0634a5c918ce2726b68ffba5a8d4fcd
Jon Clayden <jon.clayden+rniftyreg@gmail.com>
2013-06-28 00:00:00 +0000

    version 1.1.0

--- R/xform.R
+++ R/xform.R
@@ -1,11 +1,10 @@
 xformToAffine <- function (image, useQuaternionFirst = TRUE)
 {
-    if (!is.nifti(image))
-        report(OL$Error, "The specified image is not a \"nifti\" object")
+    image <- as(image, "nifti")
     
     # With no information, assume Analyze orientation and zero origin
     if (image@qform_code <= 0 && image@sform_code <= 0)
-        return (diag(c(-1, 1, 1, 1)))
+        matrix <- diag(c(-1, 1, 1, 1))
     else if ((useQuaternionFirst && image@qform_code > 0) || image@sform_code <= 0)
     {
         matrix <- diag(4)
@@ -27,14 +26,13 @@ xformToAffine <- function (image, useQuaternionFirst = TRUE)
                              2*q[3]*q[4] - 2*q[1]*q[2],
                              q[1]*q[1] + q[4]*q[4] - q[3]*q[3] - q[2]*q[2])
         
-        # The qfactor should be stored as 1 or -1, but the NIfTI standard says
-        # 0 should be treated as 1
-        # This formulation does that (the 0.1 is arbitrary)
+        # The qfactor should be stored as 1 or -1, but the NIfTI standard says 0
+        # should be treated as 1: this formulation does that (the 0.1 is arbitrary)
         qfactor <- sign(image@pixdim[1] + 0.1)
         matrix[1:3,1:3] <- matrix[1:3,1:3] * rep(c(abs(image@pixdim[2:3]), qfactor*abs(image@pixdim[4])), each=3)
-        
-        return (matrix)
     }
     else
-        return (rbind(image@srow_x, image@srow_y, image@srow_z, c(0,0,0,1)))
+        matrix <- rbind(image@srow_x, image@srow_y, image@srow_z, c(0,0,0,1))
+    
+    return (matrix)
 }
